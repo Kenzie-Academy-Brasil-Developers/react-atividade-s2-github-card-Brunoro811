d9ec8c1c9c2b891c46484135f5ab9cea6f6977de
Bruno <ferreira.bruno.oliveira@gmail.com>
2021-09-15 21:27:11 -0300

    fix: finalizado componente card

--- src/Components/List/index.jsx
+++ src/Components/List/index.jsx
@@ -1,62 +1,13 @@
 import "../Font";
-import { Grid, Paper } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-const useStyle = makeStyles((theme) => ({
-  container: {
-    width: "95%",
-    maxWidth: "400px",
-    padding: "10px",
-    boxSizing: "border-box",
-    margin: "0 auto",
-    marginBottom: "5px",
-  },
-  container__row: {
-    width: "100%",
-    display: "flex",
-    flexDirection: "row",
-  },
-  col__information: {
-    padding: "5px",
-    textAlign: "start",
-  },
-  col__img: {
-    padding: "5px",
-  },
-  img: {
-    width: "50px",
-  },
-  p: {
-    color: "#7d7d7d",
-  },
-  title: {
-    color: "#2e2f31",
-  },
-}));
-
+import { Grid } from "@material-ui/core";
+import Card from "../Card";
 function List({ arrRepo }) {
-  const classe = useStyle();
   return (
     <Grid container spacing={1}>
       <Grid item xs={12}>
-        {arrRepo.map((element) => {
-          return (
-            <Paper className={classe.container}>
-              <Grid className={classe.container__row} item xs={12}>
-                <Grid className={classe.col__img} item xs={3}>
-                  <img
-                    className={classe.img}
-                    src={element.owner.avatar_url}
-                    alt={element.full_name}
-                  />
-                </Grid>
-                <Grid className={classe.col__information} item xs={10}>
-                  <h5 className={classe.title}>{element.full_name}</h5>
-                  <p className={classe.p}>{element.description}</p>
-                </Grid>
-              </Grid>
-            </Paper>
-          );
-        })}
+        {arrRepo.map((element) => (
+          <Card key={element.id} element={element} />
+        ))}
       </Grid>
     </Grid>
   );
